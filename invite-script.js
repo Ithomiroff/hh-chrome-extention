3bcd8d0e3a51fd8cc3be8daeffeb6a197fbacbf1
Victor Tihomirov <ithomiroff@gmail.com>
2019-12-16 23:30:10 +0300

    add store

--- invite-script.js
+++
@@ -1,6 +0,0 @@
-// data-qa="resume__invite-button"
-// bloko-button
-(() => {
-    const btn = document.querySelector('.bloko-button[data-qa="resume__invite-button"]');
-    console.warn(btn);
-})();
